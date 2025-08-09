a793c21333ce672317470d73bfff843d74b3cc5c
Hadi Razal <hadhirasal22@gmail.com>
2025-08-09 21:36:01 +0400

    added user and comment models

---
+++ app/Models/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Database\MongoDB\Model;
+
+class Post extends Model
+{
+
+    protected $connection = 'mongodb';
+
+    protected $table = 'posts';
+
+
+    protected $fillable = [
+        'image_path',
+        'caption',
+        'image_url',
+        'user_id'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
+
+    
+}
